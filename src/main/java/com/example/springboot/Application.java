41815c79e03843b74213281718a75ef0a3c65420
yv88john <yv88john@gmail.com>
2020-12-29 08:33:12 +0000

    Add springboot controller mapping

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,16 +1,19 @@
 package com.example.springboot;
 
-import java.util.Arrays;
 
+import com.example.restservice.Greeting;
+import com.example.restservice.GreetingController;
+import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication(scanBasePackages = { "com.example.*"} )
 
+public class Application {
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
